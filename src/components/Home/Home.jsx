4abe24a5ed88a66d7258f87a129b7990cbc5a861
samoteur13 <s.chabani13@gmail.com>
2023-11-04 12:33:55 +0100

    talwind and structur

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.css";
+
+export default function Home() {
+  return (
+    <section className="home min-h-screen w-full flex items-center justify-center">
+      <div>
+        <h1 className="text-9xl">My name</h1>
+        <h2>my text animate</h2>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <aside className="sidebar w-full hidden xl:block min-h-screen">
+    </aside>
+  );
+}
